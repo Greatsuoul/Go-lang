40b685cc1859761f59e1b9312e8d867ff4aeb9a4
Techie_BEee <147028563+Greatsuoul@users.noreply.github.com>
2025-05-13 10:57:56 +0530

    Update 1.go

--- 1day/1.go
+++ 1day/1.go
@@ -1,9 +1,36 @@
 package main
+
 import "fmt"
-func main(){
-	name,sname := "Prasad","Kanade"
-	fmt.Println("Hello World")
-	fmt.Println(1+1)
-	fmt.Printf("My name is %s",name)
-	fmt.Println(sname)
-} 
+
+func main() {
+	// Implicit declaration
+	name := "Prasad"
+	s := 10
+	d := 6.88
+
+	// Explicit declaration
+	var sname string = "Kanade"
+	var a int = 20
+	var b float64 = 3.88
+
+	// Taking input from user
+	var age int
+	fmt.Println("Enter the age:-")
+	fmt.Scanf("%d", &age)
+
+	// Printing of data
+
+	fmt.Printf("My name is %s\n Surname is %s\n %d years old\n", name, sname, age)
+
+	//Printing values with some mathematical operation
+	fmt.Println(s + a)
+	fmt.Println(s - a)
+	fmt.Println(s * a)
+	fmt.Println(s / a)
+	fmt.Println(s % a)
+	// float operations
+
+	fmt.Println(b + d)
+	fmt.Println(b / d)
+
+}
