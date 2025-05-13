eff3ad7307bc0937486111b429b693826aa6c755
Techie_BEee <147028563+Greatsuoul@users.noreply.github.com>
2025-05-13 10:25:44 +0530

    Update 1.go

--- 1day/1.go
+++ 1day/1.go
@@ -1,6 +1,9 @@
 package main
 import "fmt"
 func main(){
+	name,sname := "Prasad","Kanade"
 	fmt.Println("Hello World")
 	fmt.Println(1+1)
+	fmt.Printf("My name is %s",name)
+	fmt.Println(sname)
 } 
